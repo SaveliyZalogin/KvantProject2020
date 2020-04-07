79847272842c3d40486d0f2f0e00e5332b74d4f1
SaveliyZalogin <born120706@gmail.com>
2020-04-07 20:10:48 +0700

    asd

--- KvantProject2020/settings.py
+++ KvantProject2020/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'KvantProject2020.mainapp'
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "KvantProject2020/static"),
+]
